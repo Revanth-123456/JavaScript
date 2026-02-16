e831806a850171668b3620d80b969f2e33d7391d
Revanth-123456 <revanthvs04@gmail.com>
2026-02-16 21:24:10 +0530

    commit

--- pattern.js
+++ pattern.js
@@ -5,3 +5,30 @@
 // console.log("*");
 // }
 
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let str = "";
+    str += "* ".repeat(i);
+    console.log(str);
+}
+
+
+let ni = 5;
+
+for (let i = 1; i <= ni; i++) {
+    let str = "";
+    str += "  ".repeat(ni - i);  // spaces
+    str += "* ".repeat(i);      // stars
+    console.log(str);
+}
+
+
+let no = 5;
+
+for (let i = 1; i <= no; i++) {
+    let str = "";
+    str += " ".repeat((no - i) * 2); // extra spaces
+    str += "* ".repeat(i);
+    console.log(str);
+}
