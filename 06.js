37fe44c5dc6dc6c5cfbf1a19fd0276ab12bb6c25
Revanth-123456 <revanthvs04@gmail.com>
2026-02-20 09:28:34 +0530

    commit

---
+++ 06.js
@@ -0,0 +1,22 @@
+// write a js program anagram means cat-act silent-listen formed by same letters
+// 2 strings
+// Write a JS program to check Anagram
+
+// Anagram program (Node.js version)
+
+let input1 = "cat";
+let input2 = "Act";
+
+function isAnagram(str1, str2) {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+
+    return str1.split("").sort().join("") === 
+           str2.split("").sort().join("");
+}
+
+if (isAnagram(input1, input2)) {
+    console.log("It is an Anagram");
+} else {
+    console.log("Not an Anagram");
+}
