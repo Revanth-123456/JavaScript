684f68f9aa4fb65c68b886f0ff32356f63647579
Revanth-123456 <revanthvs04@gmail.com>
2026-03-04 09:38:18 +0530

    commit

---
+++ 19.js
@@ -0,0 +1,66 @@
+function orderFood() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let haveOrdered = true;
+            if (haveOrdered) {
+                resolve("Recieved an order");
+            } else {
+                reject("unable to order at the moment");
+            }
+        },7000);
+    });
+}
+
+function prepareFood() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let isPrepared = true;
+            if (isPrepared) {
+                resolve("Preparing the food ");
+            } else {
+                reject("unable to prepare");
+            }
+        },4000);
+    });
+}
+
+function findDeliveryPartner() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let isFound = false;
+            if (isFound) {
+                resolve("finding delivery partner ");
+            } else {
+                reject("unable to find partner");
+            }
+        },2000);
+    });
+}
+
+function foodDelivered() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let isDelivered = true;
+            if (isDelivered) {
+                resolve("food is deliverd ");
+            } else {
+                reject("food is not delivered");
+            }
+        },1000);
+    });
+}
+
+orderFood().then((messsage)=>{
+    console.log(messsage);
+    return prepareFood();
+}).then((messsage)=>{
+    console.log(messsage);
+    return findDeliveryPartner();
+}).then((messsage)=>{
+    console.log(messsage);
+    return foodDelivered();
+}).then((messsage)=>{
+    console.log(messsage);
+}).catch((error)=>{
+    console.log("Failed ",error);
+});
